1ff1423de40ddf3307ad74eea9672359e06caf1c
lightsource <142080508+l1ghtsource@users.noreply.github.com>
2024-04-21 12:13:42 +0300

    new api

--- app/api.py
+++ app/api.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
 from pydantic import BaseModel
 from ner import ner
 from sentiment import sentiment
 from tag_prediction import get_tag
+from summarization import summarize
 
 app = FastAPI()
 
@@ -11,26 +13,27 @@ class InputText(BaseModel):
 
 class NLPResult(BaseModel):
     tag: str
-    keywords: str
+    summarization: str
     sentiment_result: str
     person: list
     location: list
     organization: list
 
+
 @app.post("/analyze/", response_model=NLPResult)
 async def analyze_text(input_text: InputText):
     text = input_text.text
     tag = get_tag(text)
-    keywords = 'слова, слова, слова'
+    summarization = summarize(text, n_words=10)  
     sentiment_result = sentiment(text)
     ner_results = ner(text)
     
-    PER = ner_results['PER']
-    LOC = ner_results['LOC']
-    ORG = ner_results['ORG']
+    PER = set([res.title() for res in ner_results['PER']])
+    LOC = set([res.title() for res in ner_results['LOC']])
+    ORG = set([res.title() for res in ner_results['ORG']])
 
     return {'tag': tag,
-            'keywords': keywords,
+            'summarization': summarization,
             'sentiment_result': sentiment_result,
             'person': PER,
             'location': LOC,
@@ -42,11 +45,10 @@ if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
-from fastapi.openapi.docs import get_swagger_ui_html
-
 @app.get("/docs/", include_in_schema=False)
 async def custom_swagger_ui_html():
     return get_swagger_ui_html(openapi_url="/openapi.json", title="API Documentation", oauth2_redirect_url="/docs")
+
 
 @app.get("/openapi.json", include_in_schema=False)
 async def get_openapi_json():
