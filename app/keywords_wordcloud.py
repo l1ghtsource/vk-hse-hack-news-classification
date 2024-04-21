40f6e93be6772802953d03763c48b29b44848cd9
lightsource <142080508+l1ghtsource@users.noreply.github.com>
2024-04-21 12:17:08 +0300

    add keywords_wordcloud

---
+++ app/keywords_wordcloud.py
@@ -0,0 +1,43 @@
+from wordcloud import WordCloud 
+import matplotlib.pyplot as plt 
+import yake
+import streamlit as st
+import io
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+russian_stopwords = stopwords.words('russian')
+stopwords = set(russian_stopwords + ['также', 'однако', 'это', 'словам', 'кроме', 'Также', 'Однако', 'Это', 'Словам', 'Кроме'])
+
+yake_kw = yake.KeywordExtractor() 
+
+
+def show_keywords_cloud(text):
+    
+    KeyWords = yake_kw.extract_keywords(text) 
+    keywords = [kw for kw, _ in KeyWords] 
+    
+    wordcloud = WordCloud(width = 400, height = 400,
+                    background_color ='white',
+                    stopwords = stopwords,
+                    min_font_size = 10).generate(' '.join(keywords))
+                        
+    plt.figure(figsize = (4, 4), facecolor = None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad = 0)
+
+    img_buffer = io.BytesIO()
+    plt.savefig(img_buffer, format='png', bbox_inches='tight', pad_inches=0)
+    img_buffer.seek(0)
+    
+    st.image(img_buffer, caption='Облако ключевых слов')
+
+
+def get_keywords(text):
+    KeyWords = yake_kw.extract_keywords(text) 
+    keywords = [kw for kw, _ in KeyWords]
+
+    return ' '.join(keywords[:1])
