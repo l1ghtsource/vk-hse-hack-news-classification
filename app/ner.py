00d23696cd5260cbdd110c4e776b0ab2a0e6a60e
lightsource <142080508+l1ghtsource@users.noreply.github.com>
2024-04-21 12:14:00 +0300

    new ner

--- app/ner.py
+++ app/ner.py
@@ -1,11 +1,43 @@
 import spacy
+from natasha import (
+    Segmenter,
+    NewsEmbedding,
+    NewsMorphTagger,
+    NewsSyntaxParser,
+    MorphVocab,
+    Doc
+)
+
+segmenter = Segmenter()
+morph_vocab = MorphVocab()
+emb = NewsEmbedding()
+
+morph_tagger = NewsMorphTagger(emb)
+syntax_parser = NewsSyntaxParser(emb)
+
+def normalize(text):
+    doc = Doc(text)
+
+    doc.segment(segmenter)
+    doc.tag_morph(morph_tagger)
+    doc.parse_syntax(syntax_parser)
+
+    for token in doc.tokens:
+        token.lemmatize(morph_vocab)
+        
+    return ' '.join(x.lemma for x in doc.tokens)
+
 
 model = spacy.load('ru_core_news_lg')
+
 
 def ner(text):
     result = {'PER': [], 'ORG': [], 'LOC': []}
 
     for ent in model(text).ents:
-        result[ent.label_] += [ent.text]
+        if ent.label_ == 'LOC':
+            result[ent.label_] += [ent.text]
+        else:
+            result[ent.label_] += [normalize(ent.text)]
     
     return result
