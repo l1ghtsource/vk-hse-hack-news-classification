8ba6eaf4acf2e9db0216617775bbcd8d3b36e020
lightsource <142080508+l1ghtsource@users.noreply.github.com>
2024-04-21 16:18:16 +0300

    add parser

---
+++ app/parser.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def parse(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        script_tag = soup.find('script', string=lambda text: 'articleBody' in text)
+        
+        if script_tag:
+            desc = script_tag.text.split('articleBody')[0].split('"description":')[1].split('genre')[0]
+            text = script_tag.text.split('articleBody')[1].split('@')[0]
+            return desc + text
+    return ' '
