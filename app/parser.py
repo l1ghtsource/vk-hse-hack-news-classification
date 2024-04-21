87ad2759d068f4f121b143f278639b602bfb532d
lightsource <142080508+l1ghtsource@users.noreply.github.com>
2024-04-21 16:30:12 +0300

    fix parser

--- app/parser.py
+++ app/parser.py
@@ -3,13 +3,18 @@ from bs4 import BeautifulSoup
 
 
 def parse(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        script_tag = soup.find('script', string=lambda text: 'articleBody' in text)
-        
-        if script_tag:
-            desc = script_tag.text.split('articleBody')[0].split('"description":')[1].split('genre')[0]
-            text = script_tag.text.split('articleBody')[1].split('@')[0]
-            return desc + text
-    return ' '
+    
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            script_tag = soup.find('script', string=lambda text: 'articleBody' in text)
+            
+            if script_tag:
+                desc = script_tag.text.split('articleBody')[0].split('"description":')[1].split('genre')[0]
+                text = script_tag.text.split('articleBody')[1].split('@')[0]
+                return desc + text
+        else:
+            return 'До связи'
+    except:
+        return 'До связи'
